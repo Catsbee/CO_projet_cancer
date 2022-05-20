b5ac8ecb89f9185b2047f81641b50e50c1f76f40
Charlotte <charlotte.mauvezin@gmail.com>
2022-05-20 19:09:10 +0200

    logistic regression + data

---
+++ main.py
@@ -0,0 +1,12 @@
+# Main script
+from sklearn.model_selection import train_test_split
+from src import *
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
---
+++ src/logistic_regression.py
@@ -0,0 +1,44 @@
+# Logistic regression python file
+# Contains all the function to do a logistic regression from scratch
+
+import numpy as np
+
+
+class LogisticRegression:
+    def __init__(self, nb_it, lr, X, Y):  # number opf iteration, learning rate, X, Y
+        self.iterations = nb_it
+        self.learning_rate = lr
+        self.nb_training_exemple, self.nb_feature = X.shape
+        self.weight = np.zeros(self.nb_feature)
+        self.bias = 0
+        self.X = X
+        self.Y = Y
+
+        def update_weight():
+            A = 1/(1+np.exp(-(self.X.dot(self.weight)+self.bias)))
+
+            # Calculation of the gradient
+            temp = np.reshape(A-self.Y.T, self.nb_training_exemple)
+            dW = np.dot(self.X.T, temp)/self.nb_training_exemple
+            db = np.sum(temp)/self.nb_training_exemple
+
+            # New weight
+            self.weight -= self.learning_rate * dW
+            self.bias -= self.learning_rate * db
+
+        for i in range(self.iterations):
+            update_weight()
+
+    def predict(self, X):
+        Z = 1/(1+np.exp(-(X.dot(self.weight)+self.bias)))
+        Y = np.where(Z > 0.5, 1, 0)
+        return Y
+
+    def sigmoid(self, z):
+        return 1/(1+np.exp(-z))
+
+    def loss(self, h, y):
+        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+
+
+
